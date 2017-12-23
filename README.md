87eff77c10b8b0fe77401c8d342b5939f899a5e9
Johnny Magrippis <j@magrippis.com>
2017-12-23 07:30:39 +0000

    Scaffolds project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Will you switch
+
+An app trying to showcase the [Monty Hall problem][].
+
+[monty hall problem]: https://en.wikipedia.org/wiki/Monty_Hall_problem "The Monty Hall Problem"
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>The Monty Hall Problem</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
